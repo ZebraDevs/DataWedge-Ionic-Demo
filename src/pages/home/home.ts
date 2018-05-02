1407b14ba8cb79c277dc180d13a7d171a27aeef5
darryncampbell <darryncampbell@hotmail.com>
2018-05-02 08:54:40 +0100

    Adding platform.ready() around the constructor code in home.ts and barcode.ts as suggested at https://github.com/Zebra/ZebraIonicDemo/issues/1#issuecomment-385888391

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -42,160 +42,164 @@ export class HomePage {
     public events: Events, private changeDetectorRef: ChangeDetectorRef, private device: Device,
     private alertController: AlertController, private platform: Platform, private toastController: ToastController) {
 
-    //  Check manufacturer.  Exit if this app is not running on a Zebra device
-    console.log("Device manufacturer is: " + this.device.manufacturer);
-    if (!(this.device.manufacturer.toLowerCase().includes("zebra"))) {
-      let alert = this.alertController.create({
-        title: 'Requires Zebra device',
-        subTitle: 'This application requires a Zebra mobile device in order to run',
-        cssClass: 'nonZebraAlert',
-        buttons: [{
-          text: 'Close app',
-          handler: data => {
-            console.log('Closing application since we are not running on a Zebra device');
-            platform.exitApp();
-          }
-        }]
+    this.platform.ready().then((readySource) => {
+
+      //  Check manufacturer.  Exit if this app is not running on a Zebra device
+      console.log("Device manufacturer is: " + this.device.manufacturer);
+      if (!(this.device.manufacturer.toLowerCase().includes("zebra"))) {
+        let alert = this.alertController.create({
+          title: 'Requires Zebra device',
+          subTitle: 'This application requires a Zebra mobile device in order to run',
+          cssClass: 'nonZebraAlert',
+          buttons: [{
+            text: 'Close app',
+            handler: data => {
+              console.log('Closing application since we are not running on a Zebra device');
+              platform.exitApp();
+            }
+          }]
+        });
+        alert.present();
+      }
+
+      //  Determine the version.  We can add additional functionality if a more recent version of the DW API is present
+      this.barcodeProvider.sendCommand("com.symbol.datawedge.api.GET_VERSION_INFO", "");
+
+      ////////////////////////////
+      //  EVENT HANDLING
+      ////////////////////////////
+
+      //  6.3 DataWedge APIs are available
+      events.subscribe('status:dw63ApisAvailable', (isAvailable) => {
+        console.log("DataWedge 6.3 APIs are available");
+        //  We are able to create the profile under 6.3.  If no further version events are received, notify the user
+        //  they will need to create the profile manually
+        this.barcodeProvider.sendCommand("com.symbol.datawedge.api.CREATE_PROFILE", "ZebraIonicDemo");
+        this.dataWedgeVersion = "6.3.  Please configure profile manually.  See the ReadMe for more details.";
+
+        //  Although we created the profile we can only configure it with DW 6.4.
+        this.barcodeProvider.sendCommand("com.symbol.datawedge.api.GET_ACTIVE_PROFILE", "");
+
+        //  Enumerate the available scanners on the device
+        this.barcodeProvider.sendCommand("com.symbol.datawedge.api.ENUMERATE_SCANNERS", "");
+
+        //  Functionality of the FAB is available so display the button
+        this.uiHideFloatingActionButton = false;
+
+        this.changeDetectorRef.detectChanges();
       });
-      alert.present();
-    }
 
-    //  Determine the version.  We can add additional functionality if a more recent version of the DW API is present
-    this.barcodeProvider.sendCommand("com.symbol.datawedge.api.GET_VERSION_INFO", "");
+      //  6.4 Datawedge APIs are available
+      events.subscribe('status:dw64ApisAvailable', (isAvailable) => {
+        console.log("DataWedge 6.4 APIs are available");
 
-    ////////////////////////////
-    //  EVENT HANDLING
-    ////////////////////////////
+        //  Documentation states the ability to set a profile config is only available from DW 6.4.
+        //  For our purposes, this includes setting the decoders and configuring the associated app / output params of the profile.
+        this.dataWedgeVersion = "6.4";
+        this.uiDatawedgeVersionAttention = false;
+        this.uiHideDecoders = !isAvailable;
 
-    //  6.3 DataWedge APIs are available
-    events.subscribe('status:dw63ApisAvailable', (isAvailable) => {
-      console.log("DataWedge 6.3 APIs are available");
-      //  We are able to create the profile under 6.3.  If no further version events are received, notify the user
-      //  they will need to create the profile manually
-      this.barcodeProvider.sendCommand("com.symbol.datawedge.api.CREATE_PROFILE", "ZebraIonicDemo");
-      this.dataWedgeVersion = "6.3.  Please configure profile manually.  See the ReadMe for more details.";
+        //  Configure the created profile (associated app and keyboard plugin)
+        let profileConfig = {
+          "PROFILE_NAME": "ZebraIonicDemo",
+          "PROFILE_ENABLED": "true",
+          "CONFIG_MODE": "UPDATE",
+          "PLUGIN_CONFIG": {
+            "PLUGIN_NAME": "BARCODE",
+            "RESET_CONFIG": "true",
+            "PARAM_LIST": {}
+          },
+          "APP_LIST": [{
+            "PACKAGE_NAME": "com.zebra.zebraionicdemo",
+            "ACTIVITY_LIST": ["*"]
+          }]
+        };
+        this.barcodeProvider.sendCommand("com.symbol.datawedge.api.SET_CONFIG", profileConfig);
 
-      //  Although we created the profile we can only configure it with DW 6.4.
-      this.barcodeProvider.sendCommand("com.symbol.datawedge.api.GET_ACTIVE_PROFILE", "");
-
-      //  Enumerate the available scanners on the device
-      this.barcodeProvider.sendCommand("com.symbol.datawedge.api.ENUMERATE_SCANNERS", "");
-
-      //  Functionality of the FAB is available so display the button
-      this.uiHideFloatingActionButton = false;
-
-      this.changeDetectorRef.detectChanges();
-    });
-
-    //  6.4 Datawedge APIs are available
-    events.subscribe('status:dw64ApisAvailable', (isAvailable) => {
-      console.log("DataWedge 6.4 APIs are available");
-
-      //  Documentation states the ability to set a profile config is only available from DW 6.4.
-      //  For our purposes, this includes setting the decoders and configuring the associated app / output params of the profile.
-      this.dataWedgeVersion = "6.4";
-      this.uiDatawedgeVersionAttention = false;
-      this.uiHideDecoders = !isAvailable;
-
-      //  Configure the created profile (associated app and keyboard plugin)
-      let profileConfig = {
-        "PROFILE_NAME": "ZebraIonicDemo",
-        "PROFILE_ENABLED": "true",
-        "CONFIG_MODE": "UPDATE",
-        "PLUGIN_CONFIG": {
-          "PLUGIN_NAME": "BARCODE",
-          "RESET_CONFIG": "true",
-          "PARAM_LIST": {}
-        },
-        "APP_LIST": [{
-          "PACKAGE_NAME": "com.zebra.zebraionicdemo",
-          "ACTIVITY_LIST": ["*"]
-        }]
-      };
-      this.barcodeProvider.sendCommand("com.symbol.datawedge.api.SET_CONFIG", profileConfig);
-
-      //  Configure the created profile (intent plugin)
-      let profileConfig2 = {
-        "PROFILE_NAME": "ZebraIonicDemo",
-        "PROFILE_ENABLED": "true",
-        "CONFIG_MODE": "UPDATE",
-        "PLUGIN_CONFIG": {
-          "PLUGIN_NAME": "INTENT",
-          "RESET_CONFIG": "true",
-          "PARAM_LIST": {
-            "intent_output_enabled": "true",
-            "intent_action": "com.zebra.ionicdemo.ACTION",
-            "intent_delivery": "2"
+        //  Configure the created profile (intent plugin)
+        let profileConfig2 = {
+          "PROFILE_NAME": "ZebraIonicDemo",
+          "PROFILE_ENABLED": "true",
+          "CONFIG_MODE": "UPDATE",
+          "PLUGIN_CONFIG": {
+            "PLUGIN_NAME": "INTENT",
+            "RESET_CONFIG": "true",
+            "PARAM_LIST": {
+              "intent_output_enabled": "true",
+              "intent_action": "com.zebra.ionicdemo.ACTION",
+              "intent_delivery": "2"
+            }
           }
-        }
-      };
-      this.barcodeProvider.sendCommand("com.symbol.datawedge.api.SET_CONFIG", profileConfig2);
+        };
+        this.barcodeProvider.sendCommand("com.symbol.datawedge.api.SET_CONFIG", profileConfig2);
 
-      //  Give some time for the profile to settle then query its value
-      setTimeout(function () {
-        barcodeProvider.sendCommand("com.symbol.datawedge.api.GET_ACTIVE_PROFILE", "");
-      }, 1000);
+        //  Give some time for the profile to settle then query its value
+        setTimeout(function () {
+          barcodeProvider.sendCommand("com.symbol.datawedge.api.GET_ACTIVE_PROFILE", "");
+        }, 1000);
 
-      this.changeDetectorRef.detectChanges();
-    });
-
-    //  6.5 Datawedge APIs are available
-    events.subscribe('status:dw65ApisAvailable', (isAvailable) => {
-      console.log("DataWedge 6.5 APIs are available");
-
-      //  The ability to switch to a new scanner is only available from DW 6.5 onwards
-      //  Reconfigure UI so the user can choose the desired scanner
-      this.uiHideSelectScanner = false;
-      this.uiHideShowAvailableScanners = true;
-
-      //  6.5 also introduced messages which are received from the API to indicate success / failure
-      this.uiHideCommandMessages = false;
-      this.barcodeProvider.requestResult(true);
-      this.dataWedgeVersion = "6.5 or higher";
-      this.changeDetectorRef.detectChanges();
-    });
-
-    //  Response to our request to find out the active DW profile
-    events.subscribe('data:activeProfile', (activeProfile) => {
-      console.log("Active profile: " + activeProfile);
-
-      //  Update the UI
-      this.activeProfileText = activeProfile;
-
-      this.changeDetectorRef.detectChanges();
-    });
-
-    //  The result (success / failure) of our last API call along with additional information
-    events.subscribe('data:commandResult', (commandResult) => {
-      this.commandResultText = commandResult;
-      this.changeDetectorRef.detectChanges();
-    });
-
-    //  Response to our requet to enumerte the scanners
-    events.subscribe('data:enumeratedScanners', (enumeratedScanners) => {
-      //  Maintain two lists, the first for devices which support DW 6.5+ and shows a combo box to select
-      //  the scanner to use.  The second will just display the available scanners in a list and be available
-      //  for 6.4 and below
-      this.scanners = enumeratedScanners;
-      this.scanners.unshift({"SCANNER_NAME": "Please Select...", "SCANNER_INDEX":-1, "SCANNER_CONNECTION_STATE":false});
-      let humanReadableScannerList = "";
-      enumeratedScanners.forEach((scanner, index) => {
-        console.log("Scanner found: name= " + scanner.SCANNER_NAME + ", id=" + scanner.SCANNER_INDEX + ", connected=" + scanner.SCANNER_CONNECTION_STATE);
-        humanReadableScannerList += scanner.SCANNER_NAME;
-        if (index < enumeratedScanners.length - 1)
-          humanReadableScannerList += ", ";
+        this.changeDetectorRef.detectChanges();
       });
-      this.availableScannersText = humanReadableScannerList;
-      this.changeDetectorRef.detectChanges();
-    });
 
-    //  A scan has been received
-    events.subscribe('data:scan', (scanData, time) => {
-      //  Update the list of scanned barcodes
-      let scannedData = scanData.extras["com.symbol.datawedge.data_string"];
-      let scannedType = scanData.extras["com.symbol.datawedge.label_type"];
-      this.scans.unshift({ "data": scannedData, "type": scannedType, "timeAtDecode": time });
-      this.changeDetectorRef.detectChanges();
+      //  6.5 Datawedge APIs are available
+      events.subscribe('status:dw65ApisAvailable', (isAvailable) => {
+        console.log("DataWedge 6.5 APIs are available");
+
+        //  The ability to switch to a new scanner is only available from DW 6.5 onwards
+        //  Reconfigure UI so the user can choose the desired scanner
+        this.uiHideSelectScanner = false;
+        this.uiHideShowAvailableScanners = true;
+
+        //  6.5 also introduced messages which are received from the API to indicate success / failure
+        this.uiHideCommandMessages = false;
+        this.barcodeProvider.requestResult(true);
+        this.dataWedgeVersion = "6.5 or higher";
+        this.changeDetectorRef.detectChanges();
+      });
+
+      //  Response to our request to find out the active DW profile
+      events.subscribe('data:activeProfile', (activeProfile) => {
+        console.log("Active profile: " + activeProfile);
+
+        //  Update the UI
+        this.activeProfileText = activeProfile;
+
+        this.changeDetectorRef.detectChanges();
+      });
+
+      //  The result (success / failure) of our last API call along with additional information
+      events.subscribe('data:commandResult', (commandResult) => {
+        this.commandResultText = commandResult;
+        this.changeDetectorRef.detectChanges();
+      });
+
+      //  Response to our requet to enumerte the scanners
+      events.subscribe('data:enumeratedScanners', (enumeratedScanners) => {
+        //  Maintain two lists, the first for devices which support DW 6.5+ and shows a combo box to select
+        //  the scanner to use.  The second will just display the available scanners in a list and be available
+        //  for 6.4 and below
+        this.scanners = enumeratedScanners;
+        this.scanners.unshift({"SCANNER_NAME": "Please Select...", "SCANNER_INDEX":-1, "SCANNER_CONNECTION_STATE":false});
+        let humanReadableScannerList = "";
+        enumeratedScanners.forEach((scanner, index) => {
+          console.log("Scanner found: name= " + scanner.SCANNER_NAME + ", id=" + scanner.SCANNER_INDEX + ", connected=" + scanner.SCANNER_CONNECTION_STATE);
+          humanReadableScannerList += scanner.SCANNER_NAME;
+          if (index < enumeratedScanners.length - 1)
+            humanReadableScannerList += ", ";
+        });
+        this.availableScannersText = humanReadableScannerList;
+        this.changeDetectorRef.detectChanges();
+      });
+
+      //  A scan has been received
+      events.subscribe('data:scan', (scanData, time) => {
+        //  Update the list of scanned barcodes
+        let scannedData = scanData.extras["com.symbol.datawedge.data_string"];
+        let scannedType = scanData.extras["com.symbol.datawedge.label_type"];
+        this.scans.unshift({ "data": scannedData, "type": scannedType, "timeAtDecode": time });
+        this.changeDetectorRef.detectChanges();
+      });
+
     });
 
   }
